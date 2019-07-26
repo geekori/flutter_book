0e4ad098a4062a039e732cf49e1d5b52949ec13c
lining <helloworld@126.com>
2019-07-26 08:34:19 +0800

    new

--- dart/test.dart
+++ dart/test.dart
@@ -1,17 +1,8 @@
 main() {
-  int x = 20;
-  print(x);
-  double y = 20.12;
-  print(y);
-  print(x + y);
-  y = 40;
-  //x = 12.4;
-  num k = 20;
-  k = 40.12;
-  print(k.runtimeType.toString());
-  var hex = 0xABCD;
-  print(hex);
-  var v = 1.42e5;
-  print(v);
+  int n = 20;
+  int m = 30;
+
+  var value = "n = $n, bitLength = ${n.bitLength} m = " + m.toString();
+  print(value);
 }
 
