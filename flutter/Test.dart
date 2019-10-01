750ca06893a6d8710b460205edab72404c9af444
lining <helloworld@126.com>
2019-10-01 21:47:24 +0800

    test.dart

--- flutter/Test.dart
+++ flutter/Test.dart
@@ -1,3 +1,24 @@
+class Product {
+  final String name;
+
+  static final Map<String, Product> cache = <String,Product>{};
+  factory Product(String name) {
+
+    if(cache.containsKey(name)) {
+      return cache[name];
+    } else {
+      final product = Product._internal(name);
+      cache[name] = product;
+      return product;
+    }
+  }
+  Product._internal(this.name);
+}
 void main() {
-  print("hello");
+
+  // singleton模式（单件设计模式）
+  var product1 = Product('iphone');
+  var product2 = Product('iphone');
+  print(identical(product1, product2));
+
 }
