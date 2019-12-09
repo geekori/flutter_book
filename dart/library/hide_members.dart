c73e4c48ef67c7cb511647097fda4757da061028
lining <helloworld@126.com>
2019-12-09 22:31:40 +0800

    code

---
+++ dart/library/hide_members.dart
@@ -0,0 +1,12 @@
+/*
+隐藏成员
+
+private
+
+_
+ */
+import 'private.dart';
+void main() {
+  print(greet("Mary"));
+  print(greet(""));
+}
---
+++ dart/library/refpart.dart
@@ -0,0 +1,14 @@
+/*
+引用库中的部分成员
+
+show
+ */
+import 'mylib.dart' show greet,MyClass;
+import 'mylib.dart' show MyClass;
+import 'mylib.dart' as my show MyClass;
+void main() {
+  print(greet("John"));
+  MyClass().process();
+
+  my.MyClass().process();
+}
